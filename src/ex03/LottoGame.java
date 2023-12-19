369ba8c9c269adab5f0d475028c054a7f92961bf
minmeanmin <wed1604@naver.com>
2023-12-19 12:37:35 +0900

    Lotto

---
+++ src/ex03/LottoGame.java
@@ -0,0 +1,25 @@
+package ex03;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LottoGame {
+    public static void main(String[] args) {
+        int arr[] = new int[6];
+        Random r = new Random();
+        int num;
+
+        for (int i = 0; i < 6; i++) {
+            num = r.nextInt(45)+1;
+            arr[i] = num;
+            for (int j = i-1; j >= 0; j--) {
+                if(arr[j] == num){
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
