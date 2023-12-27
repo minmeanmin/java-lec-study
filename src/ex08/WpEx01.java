c8e386e34269044002b09a5351a9676801aad042
minmeanmin <wed1604@naver.com>
2023-12-27 17:58:55 +0900

    String 클래스 공부

---
+++ src/ex08/WpEx01.java
@@ -0,0 +1,12 @@
+package ex08;
+
+public class WpEx01 {
+    public static void main(String[] args) {
+        int num = 10;
+        double d = num;
+
+        Integer n = 10;
+
+        double d2 = n.doubleValue();
+    }
+}
