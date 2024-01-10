a193f5e95e4df697db1785506ec147f12a86aba7
minmeanmin <wed1604@naver.com>
2024-01-10 11:51:11 +0900

    소켓통신(반이중)

---
+++ src/ex17/halfway/Client.java
@@ -0,0 +1,26 @@
+package ex17.halfway;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 20000);
+
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+
+            pw.println("3"); //requestMsg
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String responseMsg = br.readLine();
+            System.out.println("서버로부터 받은 메시지 : " + responseMsg);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
